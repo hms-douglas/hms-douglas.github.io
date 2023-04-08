43f7f45db50fe833607758fbc099a7f835c7968e
hms-douglas <51035259+hms-douglas@users.noreply.github.com>
2023-04-08 13:53:47 -0300

    watch face release

--- js/watchfaces.js
+++ js/watchfaces.js
@@ -1,2 +1,2 @@
 /* Douglas Silva :: Dect - 2023 */
-const Be=[[0, "DS A001", 1, "dsa001"],[1, "DS D001", 0, "dsd001"],[2, "DS H001", 2, "dsh001"]];
+const Be=[[3, "DS D002", 0, "dsd002"],[1, "DS D001", 0, "dsd001"],[0, "DS A001", 1, "dsa001"],[2, "DS H001", 2, "dsh001"]];
